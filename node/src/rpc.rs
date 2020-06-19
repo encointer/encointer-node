8342be4561fa3deb3983d716fe17c4b83258fcd7
clangenb <christian.langenbacher91@gmail.com>
2020-06-19 11:16:17 +0200

    reduced rpc extensions, only add:
    - system_accountNextIndex: gets the nonce taking into account all pending transactions.
    - payment_queryInfo: gets a transaction fee estimate

--- node/src/rpc.rs
+++ node/src/rpc.rs
@@ -2,20 +2,11 @@
 
 use std::{fmt, sync::Arc};
 
-use runtime::{
-    opaque::Block, AccountId, Balance, BlockNumber, Hash, Index, UncheckedExtrinsic,
+use encointer_node_runtime::{
+    opaque::Block, AccountId, Balance, Index, UncheckedExtrinsic,
 };
-use sc_consensus_babe::{Config, Epoch};
-use sc_consensus_babe_rpc::BabeRpcHandler;
-use sc_consensus_epochs::SharedEpochChanges;
-use sc_finality_grandpa::{SharedAuthoritySet, SharedVoterState};
-use sc_finality_grandpa_rpc::GrandpaRpcHandler;
-use sc_keystore::KeyStorePtr;
-use sc_rpc_api::DenyUnsafe;
 use sp_api::ProvideRuntimeApi;
 use sp_blockchain::{Error as BlockChainError, HeaderBackend, HeaderMetadata};
-use sp_consensus::SelectChain;
-use sp_consensus_babe::BabeApi;
 use sp_transaction_pool::TransactionPool;
 use substrate_frame_rpc_system::AccountNonceApi;
 
@@ -31,53 +22,26 @@ pub struct LightDeps<C, F, P> {
     pub fetcher: Arc<F>,
 }
 
-/// Extra dependencies for BABE.
-pub struct BabeDeps {
-    /// BABE protocol config.
-    pub babe_config: Config,
-    /// BABE pending epoch changes.
-    pub shared_epoch_changes: SharedEpochChanges<Block, Epoch>,
-    /// The keystore that manages the keys of the node.
-    pub keystore: KeyStorePtr,
-}
-
-/// Extra dependencies for GRANDPA
-pub struct GrandpaDeps {
-    /// Voting round info.
-    pub shared_voter_state: SharedVoterState,
-    /// Authority set info.
-    pub shared_authority_set: SharedAuthoritySet<Hash, BlockNumber>,
-}
 
 /// Full client dependencies.
-pub struct FullDeps<C, P, SC> {
+pub struct FullDeps<C, P> {
     /// The client instance to use.
     pub client: Arc<C>,
     /// Transaction pool instance.
     pub pool: Arc<P>,
-    /// The SelectChain Strategy
-    pub select_chain: SC,
-    /// Whether to deny unsafe calls
-    pub deny_unsafe: DenyUnsafe,
-    /// BABE specific dependencies.
-    pub babe: BabeDeps,
-    /// GRANDPA specific dependencies.
-    pub grandpa: GrandpaDeps,
 }
 
 /// Instantiate all Full RPC extensions.
-pub fn create_full<C, P, M, SC>(deps: FullDeps<C, P, SC>) -> jsonrpc_core::IoHandler<M>
+pub fn create_full<C, P, M>(deps: FullDeps<C, P>) -> jsonrpc_core::IoHandler<M>
     where
         C: ProvideRuntimeApi<Block>,
         C: HeaderBackend<Block> + HeaderMetadata<Block, Error = BlockChainError> + 'static,
         C: Send + Sync + 'static,
         C::Api: AccountNonceApi<Block, AccountId, Index>,
-        C::Api: BabeApi<Block>,
         C::Api: pallet_transaction_payment_rpc::TransactionPaymentRuntimeApi<Block, Balance, UncheckedExtrinsic>,
         <C::Api as sp_api::ApiErrorExt>::Error: fmt::Debug,
         P: TransactionPool + 'static,
         M: jsonrpc_core::Metadata + Default,
-        SC: SelectChain<Block> + 'static,
 {
     use pallet_transaction_payment_rpc::{TransactionPayment, TransactionPaymentApi};
     use substrate_frame_rpc_system::{FullSystem, SystemApi};
@@ -86,35 +50,11 @@ pub fn create_full<C, P, M, SC>(deps: FullDeps<C, P, SC>) -> jsonrpc_core::IoHan
     let FullDeps {
         client,
         pool,
-        select_chain,
-        deny_unsafe,
-        babe,
-        grandpa,
     } = deps;
-    let BabeDeps {
-        keystore,
-        babe_config,
-        shared_epoch_changes,
-    } = babe;
-    let GrandpaDeps {
-        shared_voter_state,
-        shared_authority_set,
-    } = grandpa;
 
     io.extend_with(SystemApi::to_delegate(FullSystem::new(client.clone(), pool)));
-    io.extend_with(sc_finality_grandpa_rpc::GrandpaApi::to_delegate(
-        GrandpaRpcHandler::new(shared_authority_set, shared_voter_state),
-    ));
     io.extend_with(TransactionPaymentApi::to_delegate(TransactionPayment::new(
         client.clone(),
-    )));
-    io.extend_with(sc_consensus_babe_rpc::BabeApi::to_delegate(BabeRpcHandler::new(
-        client.clone(),
-        shared_epoch_changes,
-        keystore,
-        babe_config,
-        select_chain,
-        deny_unsafe,
     )));
 
     io
