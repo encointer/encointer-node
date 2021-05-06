d719e10939fd0dc1270df22801921ead87aea6dc
Christian Langenbacher <clangenb+gh@protonmail.ch>
2021-05-06 12:39:15 +0200

    [client/bootstrap_demo_community.py] simplified code

--- client/bootstrap_demo_community.py
+++ client/bootstrap_demo_community.py
@@ -34,13 +34,35 @@ def upload(path_to_files):
         return ''
 
 
+def perform_meetup(client, cid):
+    print('Starting meetup...')
+    print('Creating claims...')
+    vote = len(accounts)
+    claim1 = client.new_claim(account1, vote, cid)
+    claim2 = client.new_claim(account2, vote, cid)
+    claim3 = client.new_claim(account3, vote, cid)
+
+    print('Signing claims...')
+    witness1_2 = client.sign_claim(account1, claim2)
+    witness1_3 = client.sign_claim(account1, claim3)
+
+    witness2_1 = client.sign_claim(account2, claim1)
+    witness2_3 = client.sign_claim(account2, claim3)
+
+    witness3_1 = client.sign_claim(account3, claim1)
+    witness3_2 = client.sign_claim(account3, claim2)
+
+    print('Sending witnesses to chain...')
+    client.register_attestations(account1, [witness2_1, witness3_1])
+    client.register_attestations(account2, [witness1_2, witness3_2])
+    client.register_attestations(account3, [witness1_3, witness2_3])
+
+
 def main(client=Client()):
-    # cid = client.new_community('test-locations-mediterranean.json')
-    cid = '41eSfKJrhrR6CYxPfUbwAN18R77WbxXoViRWQMAF4hJB'
-    print(cid)
+    cid = client.new_community('test-locations-mediterranean.json')
+    print(f'Registered community with cid: {cid}')
 
     print(client.list_communities())
-
     client.go_to_phase(CeremonyPhase.REGISTERING)
 
     # charlie has no genesis funds
@@ -51,57 +73,27 @@ def main(client=Client()):
     [client.register_participant(b, cid) for b in accounts]
 
     blocks_to_wait = 3
-    print(f"Waiting for {blocks_to_wait}, such that xt's are processed")
+    print(f"Waiting for {blocks_to_wait} blocks, such that xt's are processed...")
     client.await_block(blocks_to_wait)
 
-    print('List participants')
     print(client.list_participants(cid))
-
-    print('Going to next phase ')
     client.next_phase()
 
     print('Listing meetups')
     print(client.list_meetups(cid))
-
-    print('Going to next phase ')
     client.next_phase()
 
-    print('Starting meetup')
-    print('Creating claims')
-    vote = len(accounts)
-    claim1 = client.new_claim(account1, vote, cid)
-    claim2 = client.new_claim(account2, vote, cid)
-    claim3 = client.new_claim(account3, vote, cid)
+    perform_meetup(client, cid)
 
-    print('Signing claims')
-    witness1_2 = client.sign_claim(account1, claim2)
-    witness1_3 = client.sign_claim(account1, claim3)
-
-    witness2_1 = client.sign_claim(account2, claim1)
-    witness2_3 = client.sign_claim(account2, claim3)
-
-    witness3_1 = client.sign_claim(account3, claim1)
-    witness3_2 = client.sign_claim(account3, claim2)
-
-    print('sending witnesses to chain')
-    client.register_attestations(account1, [witness2_1, witness3_1])
-    client.register_attestations(account2, [witness1_2, witness3_2])
-    client.register_attestations(account3, [witness1_3, witness2_3])
-
-    print(f"Waiting for {blocks_to_wait}, such that xt's are processed")
+    print(f"Waiting for {blocks_to_wait} blocks, such that xt's are processed...")
     client.await_block(blocks_to_wait)
 
-    print('Listing Attestations')
     print(client.list_attestations(cid))
-
-    print('Going to next phase ')
     client.next_phase()
 
-    print(f'Balances for new community with cid: {cid}')
+    print(f'Balances for new community with cid: {cid}.')
     bal = [client.balance(a, cid=cid) for a in accounts]
-    abs = list(zip(accounts, bal))
-
-    [print(f'Account balance for {ab[0]} is {ab[1]}') for ab in abs]
+    [print(f'Account balance for {ab[0]} is {ab[1]}.') for ab in list(zip(accounts, bal))]
 
 
 if __name__ == '__main__':
