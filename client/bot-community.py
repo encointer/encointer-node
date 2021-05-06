d44b38740b0bae730f6214f558db4fbd62436304
Christian Langenbacher <clangenb+gh@protonmail.ch>
2021-05-06 10:49:15 +0200

    [client] extracted a python client out of bot-community.py

--- client/bot-community.py
+++ client/bot-community.py
@@ -8,6 +8,9 @@ import geojson
 
 from math import sqrt, floor
 from pyproj import Geod
+
+from client.client import Client
+
 geoid = Geod(ellps='WGS84')
 
 cli = ["../target/release/encointer-client-notee"]
@@ -34,73 +37,6 @@ def populate_locations(northwest, n, dist=1000):
         locations += col
     return locations
 
-def next_phase():
-    subprocess.run(cli + ["next-phase"])
-
-def get_phase():
-    ret = subprocess.run(cli + ["get-phase"], stdout=subprocess.PIPE)
-    return ret.stdout.strip().decode("utf-8")
-
-def list_accounts():
-    ret = subprocess.run(cli + ["list-accounts"], stdout=subprocess.PIPE)
-    return ret.stdout.decode("utf-8").splitlines()
-
-def new_account():
-    ret = subprocess.run(cli + ["new-account"], stdout=subprocess.PIPE)
-    return ret.stdout.decode("utf-8").strip()
-
-def faucet(accounts):
-    subprocess.run(cli + ["faucet"] + accounts, stdout=subprocess.PIPE)
-
-def balance(accounts, **kwargs):
-    bal = []
-    cid_arg = []
-    if 'cid' in kwargs:
-        cid_arg = ["--cid", kwargs.get('cid')]
-    for account in accounts:
-        ret = subprocess.run(cli + cid_arg + ["balance", account], stdout=subprocess.PIPE)
-        bal.append(float(ret.stdout.strip().decode("utf-8").split(' ')[-1]))
-    return bal
-
-def new_community(specfile):
-    ret = subprocess.run(cli + ["new-community", specfile, '//Alice'], stdout=subprocess.PIPE)
-    return ret.stdout.decode("utf-8").strip()
-
-def await_block():
-    subprocess.run(cli + ["listen", "-b", "1"], stdout=subprocess.PIPE)
-
-def register_participant(account, cid):
-    ret = subprocess.run(cli + ["--cid", cid, "register-participant", account], stdout=subprocess.PIPE)
-    #print(ret.stdout.decode("utf-8"))
-
-def new_claim(account, vote, cid):
-    ret = subprocess.run(cli + ["--cid", cid, "new-claim", account, str(vote)], stdout=subprocess.PIPE)
-    return ret.stdout.decode("utf-8").strip()
-
-def sign_claim(account, claim):
-    ret = subprocess.run(cli + ["sign-claim", account, claim], stdout=subprocess.PIPE)
-    return ret.stdout.decode("utf-8").strip()
-
-def list_meetups(cid):
-    ret = subprocess.run(cli + ["--cid", cid, "list-meetups"], stdout=subprocess.PIPE)
-    #print(ret.stdout.decode("utf-8"))
-    meetups = []
-    lines = ret.stdout.decode("utf-8").splitlines()
-    while len(lines) > 0:
-        if 'participants are:' in lines.pop(0):
-            participants = []
-            while len(lines) > 0:
-                l = lines.pop(0)
-                if 'MeetupRegistry' in l:
-                    break
-                participants.append(l.strip())
-            meetups.append(participants)
-    return meetups
-
-def register_attestations(account, attestations):
-    ret = subprocess.run(cli + ["register-attestations", account] + attestations, stdout=subprocess.PIPE)
-    #print(ret.stdout.decode("utf-8"))
-
 
 def generate_community_spec(name, locations, bootstrappers):
     meta = meta_json(name, "CSP", "Defau1tCidThat1s46Characters1nLength1111111111")
@@ -113,24 +49,24 @@ def generate_community_spec(name, locations, bootstrappers):
         geojson.dump(gj, outfile)
     return fname
 
-def random_community_spec():
+def random_community_spec(client=Client()):
     point = geojson.utils.generate_random("Point", boundingBox=[-56, 41, -21, 13])
     locations = populate_locations(point, NUMBER_OF_LOCATIONS)
     print("created " + str(len(locations)) + " random locations around " + str(point))
     bootstrappers = []
-    for bi in range(0,10):
-        bootstrappers.append(new_account())
+    for bi in range(0, 10):
+        bootstrappers.append(client.new_account())
     print('new bootstrappers:' + ' '.join(bootstrappers))
-    faucet(bootstrappers)
-    await_block()
-    name = '#' + '-'.join(RandomWords().random_words(count=2))
+    client.faucet(bootstrappers)
+    client.await_block()
+    name = '#' + '-'.join(RandomWords().random_words(count=1))
     return generate_community_spec(name, locations, bootstrappers)
 
-def init():
+def init(client=Client()):
     print("initializing community")
-    specfile = random_community_spec()
+    specfile = random_community_spec(client)
     print("generated community spec: ", specfile)
-    cid = new_community(specfile)
+    cid = client.new_community(specfile)
     print("created community with cid: ", cid)
     f = open("cid.txt", "w")
     f.write(cid)
@@ -139,16 +75,16 @@ def init():
 def meta_json(name, symbol, icons_cid):
     return { "name": name, "symbol": symbol, "icons": icons_cid }
 
-def run():
+def run(client=Client()):
     f = open("cid.txt", "r")
     cid = f.read()
     print("cid is " + cid)
-    phase = get_phase()
+    phase = client.get_phase()
     print("phase is " + phase)
-    accounts = list_accounts()
+    accounts = client.list_accounts()
     print("number of known accounts: " + str(len(accounts)))
     if phase == 'REGISTERING':
-        bal = balance(accounts, cid=cid)
+        bal = client.balance(accounts, cid=cid)
         total = sum(bal)
         print("****** money supply is " + str(total))
         f = open("bot-stats.csv", "a")
@@ -160,43 +96,44 @@ def run():
             if n_newbies > 0:
                 newbies = []
                 for n in range(0,n_newbies):
-                    newbies.append(new_account())
-                faucet(newbies)
-                await_block()
-                accounts = list_accounts()
+                    newbies.append(client.new_account())
+                client.faucet(newbies)
+                client.await_block()
+                accounts = client.list_accounts()
 
         print("registering " + str(len(accounts)) + " participants")
         for p in accounts:
             #print("registering " + p)
-            register_participant(p, cid)
-        await_block()
+            client.register_participant(p, cid)
+        client.await_block()
     if phase == 'ATTESTING':
-        meetups = list_meetups(cid)
+        meetups = client.list_meetups(cid)
         print("****** Performing " + str(len(meetups)) + " meetups")
         for meetup in meetups:
             n = len(meetup)
             print("Performing meetup with " + str(n) + " participants")
             claims = {}
             for p in meetup:
-                claims[p] = new_claim(p, n, cid)
+                claims[p] = client.new_claim(p, n, cid)
             for claimant in meetup:
                 attestations = []
                 for attester in meetup:
                     if claimant == attester:
                         continue
                     #print(claimant + " is attested by " + attester)
-                    attestations.append(sign_claim(attester, claims[claimant]))
+                    attestations.append(client.sign_claim(attester, claims[claimant]))
                 #print("registering attestations for " + claimant)
-                register_attestations(claimant, attestations)
-        await_block()
+                client.register_attestations(claimant, attestations)
+        client.await_block()
 
 def benchmark():
     print("will grow population forever")
+    client = Client()
     while True:
         run()
-        await_block()
-        next_phase()
-        await_block()
+        client.await_block()
+        client.next_phase()
+        client.await_block()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog='bot-community')
