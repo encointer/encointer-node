271463a67c0cc4acb85a08418569b0448ae9ad1b
Christian Langenbacher <clangenb+gh@protonmail.ch>
2021-08-10 07:56:35 +0200

    [client] typo

--- client/py_client/helpers.py
+++ client/py_client/helpers.py
@@ -5,7 +5,7 @@ import os
 def purge_prompt(path: str, file_description: str):
     files = glob.glob(path + '/*')
     if files:
-        print(f'Keystore already contains {len(files)} {file_description}.')
+        print(f'{path} already contains {len(files)} {file_description}.')
         should_clear = input(f'Do you want to purge the {path}? [y, n]')
         if should_clear == 'y':
             [os.remove(f) for f in files]
