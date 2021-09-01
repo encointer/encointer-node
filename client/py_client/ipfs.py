5490fa98427fbefbfcc5de6ba8afcd7195a52f10
anizeani <36300021+anizeani@users.noreply.github.com>
2021-09-01 18:37:49 +0300

    further improvements, but node raised error, when refactoring to static method

--- client/py_client/ipfs.py
+++ client/py_client/ipfs.py
@@ -3,24 +3,32 @@ import re
 import warnings
 import os
 
-local_ipfs_key = None
-
-if os.path.isfile("client\py_client\config.py"):
-    from config import loadIPFSkey
-    loadIPFSkey()
-    local_ipfs_key = os.environ['LOLCAL_IPFS_API_KEY']
-    print("local_ipfs_key: ", local_ipfs_key)
-
 ICONS_PATH = '../test-data/icons'
+
+
 class Ipfs:
     """ Minimal wrapper for the ipfs cli """
     @staticmethod
     def add_recursive(path_to_files):
         ret = subprocess.run(
             ["ipfs", "add", "-rw", path_to_files], stdout=subprocess.PIPE)
-
-        # last line contains the directory cid
         last = ret.stdout.splitlines()[-1]
+        p = re.compile('Qm\\w*')
+        cids = p.findall(str(last))
+        if cids:
+            print()
+            print(cids)
+            return cids[0]
+        else:
+            warnings.warn('No cid returned. Something happened. stderr: ')
+            warnings.warn(str(ret.stderr))
+            return ''
+        # Ipfs.refactoredCID(ret)
+
+    @staticmethod
+    def refactoredCID(ret_cid):
+        # last line contains the directory cid
+        last = ret_cid.stdout.splitlines()[-1]
         p = re.compile('Qm\\w*')
         cids = p.findall(str(last))
 
@@ -30,7 +38,7 @@ class Ipfs:
             return cids[0]
         else:
             warnings.warn('No cid returned. Something happened. stderr: ')
-            warnings.warn(str(ret.stderr))
+            warnings.warn(str(ret_cid.stderr))
             return ''
 
     @staticmethod
@@ -40,22 +48,23 @@ class Ipfs:
     @staticmethod
     def add_recursive_remote(path_to_files, IPFS_API_KEY):
         if IPFS_API_KEY != '':
-            ret = subprocess.run(
-                ["curl", "-X", "POST", "-F", f"file=@{path_to_files}", IPFS_API_KEY, "https://ipfs.infura.io:5001/api/v0/add?recursive&wrap-with-directory&quiter"], stdout=subprocess.PIPE)
-        elif local_ipfs_key != None:
-            ret = subprocess.run(
-                ["curl", "-X", "POST", "-F", f"file=@{path_to_files}", local_ipfs_key, "https://ipfs.infura.io:5001/api/v0/add"], stdout=subprocess.PIPE)
-        # last line contains the directory cid
-        last = ret.stdout.splitlines()[-1]
-        p = re.compile('Qm\\w*')
-        cids = p.findall(str(last))
-        if cids:
-            print()
-            print(cids)
-            return cids[0]
+            ret = subprocess.run(["curl", "-X", "POST", "-F", f"file=@{path_to_files}", IPFS_API_KEY,
+                                 "https://ipfs.infura.io:5001/api/v0/add?recursive&wrap-with-directory&quiter"], stdout=subprocess.PIPE)
+            last = ret.stdout.splitlines()[-1]
+            p = re.compile('Qm\\w*')
+            cids = p.findall(str(last))
+            if cids:
+                print()
+                print(cids)
+                return cids[0]
+            else:
+                warnings.warn('No cid returned. Something happened. stderr: ')
+                warnings.warn(str(ret.stderr))
+                return ''
+            # Ipfs.refactoredCID(ret)
         else:
-            warnings.warn('No cid returned. Something happened. stderr: ')
-            warnings.warn(str(ret.stderr))
+            warnings.warn('No IPFS_API_KEY entered. Please add using the --ipfs-api-key option. stderr: ')
+            # warnings.warn(str(ret.stderr))
             return ''
 
     @staticmethod
