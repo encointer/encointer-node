417b5ad2d2def1165800fa3ad9b13bb56b9e6344
eran <eranventura@gmail.com>
2021-11-29 09:32:03 +0100

    refactored phase.py to use click command

--- client/phase.py
+++ client/phase.py
@@ -13,23 +13,15 @@ import substrateinterface
 import json
 from py_client.client import Client
 
-
-def subscription_handler(event_count, update_nr, subscription_id):
-    global COUNT
-    print(f'events: {event_count}, idle blocks {COUNT}')
-    if COUNT > 10:
-        return update_nr
-    elif event_count.value == 1:
-        COUNT += 1
-    else:
-        COUNT = 0
+global COUNT
+COUNT = 0
 
 
-if __name__ == '__main__':
-    p = argparse.ArgumentParser(
-    prog='register-businesses', parents=[simple_parser()])
-    args = p.parse_args()
-    # print(f"Starting script with client '{args.client}' on port {args.port}")
+@click.command()
+@click.option('--node_url', default=None, help='if set, remote chain is used with port 443, no need to manually set port, it will be ignored')
+@click.option('--client', default='../target/release/encointer-client-notee', help='the client to communicate with the chain')
+@click.option('--port', default='9944', help='port for the client to communicate with chain')
+def main(node_url, client, port):
     localhost = None
     if node_url is None:
         client = Client(rust_client=client, port=port)
