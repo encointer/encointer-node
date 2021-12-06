a19f4915087c1ce7178288238bc1b84eec2c05d4
Christian Langenbacher <clangenb+gh@protonmail.ch>
2021-12-06 16:59:04 +0100

    [py-client] fix flag `endorsee` -> `endorsees`

--- client/py_client/client.py
+++ client/py_client/client.py
@@ -185,7 +185,7 @@ class Client:
         return ret.stdout.decode("utf-8").strip()
 
     def endorse_newcomers(self, cid, endorser, endorsees):
-        ret = subprocess.run(self.cli + ["endorse-newcomers", endorser, "--cid", cid, "--endorsee", endorsees],
+        ret = subprocess.run(self.cli + ["endorse-newcomers", endorser, "--cid", cid, "--endorsees"] + endorsees,
                              stdout=subprocess.PIPE)
         return ret.stdout.decode("utf-8").strip()
 
