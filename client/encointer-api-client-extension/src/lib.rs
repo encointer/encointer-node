a260bb0c8744ff212aee3949941c3ce5f8457312
Christian Langenbacher <clangenb+gh@protonmail.ch>
2021-12-27 14:24:28 +0100

    [client/api-client-extensions] add CommunitiesApi with get_locations

--- client/encointer-api-client-extension/src/lib.rs
+++ client/encointer-api-client-extension/src/lib.rs
@@ -1,10 +1,12 @@
 use sp_core::sr25519;
-use substrate_api_client::{rpc::WsRpcClient, ApiClientError};
+use substrate_api_client::rpc::WsRpcClient;
 
-pub type Result<T> = std::result::Result<T, ApiClientError>;
+pub use substrate_api_client::{ApiClientError, ApiResult as Result};
 
 pub type Api = substrate_api_client::Api<sr25519::Pair, WsRpcClient>;
 
 pub use ceremonies::*;
+pub use communities::*;
 
 mod ceremonies;
+mod communities;
