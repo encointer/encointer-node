4f39485b906aa5032c5de1349081d0dfa91ca672
anizeani <eranventura@gmail.com>
2022-02-21 16:38:49 +0100

    register businesses should upload businesses and offerings as files and not as folders added ipfs funcitonality to add non-recursive (without folder structure)

--- client/py_client/ipfs.py
+++ client/py_client/ipfs.py
@@ -57,3 +57,22 @@ class Ipfs:
         return [Ipfs.add_recursive(f, local) for f in paths]
 
 
+    @staticmethod
+    def add(path_to_files, local=False):
+        if not (use_ipfs_gateway or local):
+            return "QmWgTp4fBkxyUhnMrx4UVVqQ2McTQKJzq8yq3J5tCzdtfx"
+        if local:
+            ret = subprocess.run(["ipfs", "add", path_to_files], stdout=subprocess.PIPE)
+            return take_only_last_cid(ret)
+        else:
+            ret = subprocess.run(["curl", "-X", "POST", "-F", f"file=@{path_to_files}", "-u", ipfs_api_key, ipfs_add_url], stdout=subprocess.PIPE)
+        return take_only_last_cid(ret)
+
+
+    @staticmethod
+    def add_multiple(paths, local = False):
+        if not (use_ipfs_gateway or local):
+            return ["QmP2fzfikh7VqTu8pvzd2G2vAd4eK7EaazXTEgqGN6AWoD"]
+        return [Ipfs.add(f, local) for f in paths]
+
+
