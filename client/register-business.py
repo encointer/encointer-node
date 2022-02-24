379a56ce1961f7411c40175dd097c722c4b7da6f
anizeani <eranventura@gmail.com>
2022-02-24 14:56:13 +0100

    register business by selecting json file, i think its more user friendly than pasting the whole path in the terminal

--- client/register-business.py
+++ client/register-business.py
@@ -8,6 +8,7 @@ import json
 import click
 import tkinter as tk
 from tkinter import filedialog
+from py_client.helpers import set_local_or_remote_chain
 import os
 
 BUSINESSES_PATH = './test-data/bazaar/'
@@ -15,124 +16,37 @@ BUSINESSES_PATH = './test-data/bazaar/'
 # Before running this script, make sure, that a community is registered on the chain (for example by running bot-community.py init)
 
 @click.command()
+@click.option('--client', default='../target/release/encointer-client-notee', help='Client binary to communicate with the chain.')
+@click.option('--port', default='9944', help='ws-port of the chain.')
 @click.option('--cid', default='', help='the community identifier of the community your business belongs to (11 digits)')
-@click.option('-l', '--ipfs_local', is_flag=True, help='if set, local ipfs node is used.')
-def create_business(cid: str, ipfs_local):
+@click.option('-r', '--remote_chain', default=None, help='choose remote_chain: gesell.')
+def register_business(cid, client, port, remote_chain):
     """
     Register a business on chain
 
     :param name: path to LocalBusiness.json with all infos specified in https://github.com/encointer/pallets/blob/master/bazaar/README.md
     :return:
     """
+    client = set_local_or_remote_chain(client, port, remote_chain)
 
     root = tk.Tk()
     root.withdraw()
 
-    bizTitle = 'Select your business json file'
-    bizFileRead = filedialog.askopenfile(mode='r', title=bizTitle)
+    biz_title = 'Select your business json file'
+    biz_file = filedialog.askopenfile(mode='r', title=biz_title)
     # should we handle trailling commma of last element? its in general not allowed in python
-    businessPyObject = json.load(bizFileRead)
+    businessPyObject = json.load(biz_file)
+    biz_file.close()
+
     print(businessPyObject)
+    # print(type(businessPyObject))
 
-    bizImage = 'Select your business image'
-    bizImageFile = filedialog.askopenfile(mode='r', title=bizImage)
-
-    if bizImageFile:
-        logo_path = os.path.abspath(bizImageFile.name)
-        try:
-            image_cid = Ipfs.add(logo_path, ipfs_local)
-            print("image_cid is:", image_cid)
-        except:
-            print("failed to add image to ipfs")
-
-    bizImageFile.close()
-
-    businessPyObject['logo'] = image_cid
-    bizFileRead.close()
-    # handle read & write in one operation?
-    bizFileWrite = open(os.path.abspath(bizFileRead.name), 'w')
-
-    json.dump(businessPyObject, bizFileWrite, indent=2)
-    bizFileWrite.close()
-    print(type(businessPyObject))
-    print(businessPyObject)
-    return {
-        "name": businessPyObject['name'],
-        "description": businessPyObject['description'],
-        "image_cid": businessPyObject['logo']
-    }
-
-
-
-def register_business(name: str, description: str, owner, chain_local: bool, ipfs_local: bool):
-    if chain_local:
-        print("registering on local chain")
-        client = Client()
-    else:
-        print("registering on remote chain")
-        client = Client(node_url='wss://gesell.encointer.org', port=443)
-
-    cid = read_cid()
-    print("the cid is: ", cid);
-    business_json = create_business(name, description, ipfs_local)
-    f_name = f'{BUSINESSES_PATH}/{business_json["name"]}.json'
-    print(f'Dumping business {business_json} to {f_name}')
-    with open(f_name, 'w') as outfile:
-        json.dump(business_json, outfile, indent=2)
-    print("f_name, the business_dump_path: ", f_name)
-    ipfs_cid = Ipfs.add_recursive(f_name, ipfs_local)
-    print(f'Uploaded business to ipfs: ipfs_cid: {ipfs_cid}')
-    print(f"registering business on chain for cid {cid}")
-    print(client.create_business(owner, cid, ipfs_cid))
+    owner = client.new_account()
+    print('owner is:', owner)
+    client.faucet(owner)
+    print(client.create_business(owner, cid, businessPyObject['logo']))
     client.await_block()
 
-
-
-def create_offering(name: str, price: int, community_identifier, ipfs_local):
-    """
-    Create an offering.
-
-    Note:   This `Offering` format is not definite, but it does not matter for simple testing as we upload only
-            the ipfs_cid.
-    :param community_identifier:
-    :return:
-    """
-    try:
-        print("adding offering image to remote: ")
-        image_cid = Ipfs.add_recursive(ICON_PATH, ipfs_local)
-    except:
-        print("failed to add image to ipfs")
-    return {
-        "name": name,
-        "price": price,
-        "community": community_identifier,
-        "image_cid": image_cid
-    }
-
-
-
-def register_offering(name: str, price: int, owner, chain_local: bool, ipfs_local: bool):
-    if chain_local:
-        print("registering on local chain")
-        client = Client()
-    else:
-        print("registering on remote chain")
-        client = Client(node_url='wss://gesell.encointer.org')
-
-    cid = read_cid()
-
-    offering_json = create_offering(name, price, cid, ipfs_local)
-
-    f_name = f'{OFFERINGS_PATH}/{offering_json["name"]}.json'
-    print(f'Dumping offerings {offering_json} to {f_name}')
-    with open(f_name, 'w') as outfile:
-        json.dump(offering_json, outfile, indent=2)
-
-    ipfs_cid = Ipfs.add_recursive(f_name, ipfs_local)
-    print(f'Uploaded offering to ipfs: ipfs_cid: {ipfs_cid}')
-    print(f"registering offering on chain for cid {cid}")
-    print(client.create_offering(owner, cid, ipfs_cid))
-    client.await_block()
 
 if __name__ == '__main__':
-    create_business()
+    register_business()
