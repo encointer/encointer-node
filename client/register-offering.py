59244a4d0749668090fcbcb34e82aa828bd80034
anizeani <eranventura@gmail.com>
2022-02-28 10:29:04 +0100

    make community identifier and bizaccount required, comment out account creation, expect already existing account uploading product.json to ipfs verifying that name and description are present in json file add warning if logo is missing create offering and upload to ipfs

--- client/register-offering.py
+++ client/register-offering.py
@@ -18,8 +18,8 @@ BUSINESSES_PATH = './test-data/bazaar/'
 @click.command()
 @click.option('--client', default='../target/release/encointer-client-notee', help='Client binary to communicate with the chain.')
 @click.option('--port', default='9944', help='ws-port of the chain.')
-@click.option('--bizaccount', default='', help='the account of the owner in ss58 format or raw_seed.')
-@click.option('--cid', default='', help='the community identifier of the community you want to register your business in (11 digits).')
+@click.option('--bizaccount', required=True, help='the account of the owner in ss58 format or raw_seed.')
+@click.option('--cid', required=True, help='the community identifier of the community you want to register your business in (11 digits).')
 @click.option('--price', default='0', help='price of your offering.')
 @click.option('-r', '--remote_chain', default=None, help='choose remote_chain: gesell.')
 def register_offering(bizaccount, cid, price, client, port, remote_chain):
@@ -34,37 +34,50 @@ def register_offering(bizaccount, cid, price, client, port, remote_chain):
     root = tk.Tk()
     root.withdraw()
 
-    biz_title = 'Select your offering json file'
-    biz_file = filedialog.askopenfile(mode='r', title=biz_title)
-    biz_file.name
-    print(os.path.basename(biz_file.name))
-    # should we handle trailling commma of last element? its in general not allowed in python
-    businessPyObject = json.load(biz_file)
-    biz_file.close()
-    businessPyObject['price'] = price
+    product_title = 'Select your Product.json'
+    product_file = filedialog.askopenfile(mode='r', title=product_title)
+    # should we handle trailling commma of last element? its in general not allowed in python and json format should not have trailing comma
+    product = json.load(product_file)
+    print('product is:', product)
 
+    assert 'name' in product, "name must be defined in json file"
+    assert 'description' in product, "description must be defined in json file"
+    product_file.close()
+    print(f'adding product {product_file.name} to ipfs')
+    product_cid = Ipfs.add(product_file.name)
+    offering = {
+        'itemOffered': product_cid,
+        'price': price
+    }
     with tempfile.TemporaryDirectory() as tmp:
         print('created temporary directory', tmp)
-        f_name = f'{os.path.basename(biz_file.name)}_offering.json'
+        file_name_without_extension = os.path.splitext(os.path.basename(product_file.name))[0]
+        f_name = f'{tmp}/{file_name_without_extension}Offering.json'
         with open(f_name, 'w') as outfile:
-            json.dump(businessPyObject, outfile, indent=2)
-            offer_cid = Ipfs.add(outfile)
-            print("offer_cid is:", offer_cid)
-
-    print(businessPyObject)
-    # print(type(businessPyObject))
+            json.dump(offering, outfile, indent=2)
+        print(f'adding offering {f_name} to ipfs')
+        offer_cid = Ipfs.add(f_name)
 
     # if account doesn't exist yet:
-    owner = client.new_account()
-    print('owner is:', owner)
-    client.faucet(owner)
-    print(client.create_offering(owner, cid, price, businessPyObject['logo']))
-    client.await_block()
-
+    # owner = '5Hn1PiMrErPKpJC1VWSNBwSdWDoHW1XTQgcGUXD8enHpHKDo'
+    # print('owner is:', owner)
+    # client.faucet(owner)
+    # print(client.create_offering(owner, cid, product['logo']))
+    # client.await_block()
 
     # if account already exists and is fauceted:
-    # print(client.create_business(bizaccount, cid, price, businessPyObject['logo']))
-    # client.await_block()
+    print(f'registering product:')
+    print(f'    cid:            {cid}')
+    print(f'    owner:          {bizaccount}')
+    print(f'    offering url:   {offer_cid}\n')
+
+    # there is a naming mismatch: we actually register/create a product on chain and not an offering,
+    # since an offering is a product with price, where the chain doesnt store the price
+    try:
+        print(client.create_offering(bizaccount, cid, product['logo']))
+        client.await_block()
+    except:
+        print("json file doesn't have a logo entry, please save for the logo a content identifier in the json file")
 
 
 if __name__ == '__main__':
