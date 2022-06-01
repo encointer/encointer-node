acca9e280e08b09b89c7038309bb3e7583a889b8
Christian Langenbacher <clangenb+gh@protonmail.ch>
2022-06-01 11:57:25 +0200

    [client] define community currency `AssetTip`

---
+++ client/encointer-api-client-extension/src/extrinsic_params.rs
@@ -0,0 +1,47 @@
+use ac_primitives::{BaseExtrinsicParams, BaseExtrinsicParamsBuilder};
+use codec::{Compact, Decode, Encode};
+
+/// A struct representing the signed extra and additional parameters required
+/// to construct a transaction and pay in asset fees
+pub type CommunityCurrencyTipExtrinsicParams = BaseExtrinsicParams<AssetTip>;
+/// A builder which leads to [`CommunityCurrencyTipExtrinsicParams`] being constructed.
+/// This is what you provide to methods like `sign_and_submit()`.
+pub type CommunityCurrencyTipExtrinsicParamsBuilder = BaseExtrinsicParamsBuilder<AssetTip>;
+
+
+/// A tip payment made in the form of a specific asset.
+#[derive(Copy, Clone, Debug, Default, Decode, Encode, Eq, PartialEq)]
+pub struct AssetTip {
+	#[codec(compact)]
+	tip: u128,
+	asset: Option<u32>,
+}
+
+impl AssetTip {
+	/// Create a new tip of the amount provided.
+	pub fn new(amount: u128) -> Self {
+		AssetTip {
+			tip: amount,
+			asset: None,
+		}
+	}
+
+	/// Designate the tip as being of a particular asset class.
+	/// If this is not set, then the native currency is used.
+	pub fn of_asset(mut self, asset: u32) -> Self {
+		self.asset = Some(asset);
+		self
+	}
+}
+
+impl From<u128> for AssetTip {
+	fn from(n: u128) -> Self {
+		AssetTip::new(n)
+	}
+}
+
+impl From<AssetTip> for u128 {
+	fn from(tip: AssetTip) -> Self {
+		tip.tip
+	}
+}
--- client/encointer-api-client-extension/src/lib.rs
+++ client/encointer-api-client-extension/src/lib.rs
@@ -3,7 +3,7 @@ use substrate_api_client::rpc::WsRpcClient;
 
 pub use substrate_api_client::{ApiClientError, ApiResult as Result};
 
-pub type Api = substrate_api_client::Api<sr25519::Pair, WsRpcClient>;
+pub type Api = substrate_api_client::Api<sr25519::Pair, WsRpcClient, extrinsic_params::CommunityCurrencyTipExtrinsicParams>;
 
 pub use ceremonies::*;
 pub use communities::*;
@@ -12,3 +12,4 @@ pub use scheduler::*;
 mod ceremonies;
 mod communities;
 mod scheduler;
+mod extrinsic_params;
