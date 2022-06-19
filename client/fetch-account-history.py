694291cecf0f014d10b216f05788095c43534ab0
Alain Brenzikofer <alain.brenzikofer@scs.ch>
2022-06-19 11:50:41 +0200

    encointer transfers event fetch works

--- client/fetch-account-history.py
+++ client/fetch-account-history.py
@@ -7,24 +7,27 @@ import csv
 import sys
 import json
 import time
+from datetime import datetime
 from substrateinterface.utils.ss58 import ss58_decode, ss58_encode
+from base58 import b58encode
 
-if len(sys.argv) < 2:
-    print("Usage: ./fetch.account-history.py <fund index> <subscan api key>")
-    sys.exit()
+#if len(sys.argv) < 2:
+#    print("Usage: ./fetch-account-history.py <account> <subscan api key>")
+#    sys.exit()
 
-account = sys.argv[1]
-#api_key = sys.argv[2]
+#account = sys.argv[1]
+account = "all"
+api_key = sys.argv[1]
 
 page_rows = 100
 
 t_start = time.time()
 
 start_block = 535620
-end_block = 660815
+end_block = 781918 #660815
 blocks_total = end_block - start_block
 
-with open(f'account-events-{account}.csv', 'w', newline='') as csvfile:
+with open(f'account-events-{account}-{start_block}_to_{end_block}.csv', 'w', newline='') as csvfile:
     writer = csv.writer(csvfile, delimiter=',')
 
     for block in range(start_block, end_block):
@@ -33,7 +36,7 @@ with open(f'account-events-{account}.csv', 'w', newline='') as csvfile:
             response = requests.post('https://encointer.api.subscan.io/api/scan/events',
                                      headers={
                                          'Content-Type': 'application/json',
-                                         #'X-API-Key': api_key,
+                                         'X-API-Key': api_key,
                                          'Accept': 'application/json',
                                      },
                                      json={
@@ -55,9 +58,10 @@ with open(f'account-events-{account}.csv', 'w', newline='') as csvfile:
                 print("sleeping a bit")
                 time.sleep(0.9)
                 continue
-            remaining = int(response.headers['X-RateLimit-Remaining-Second'])
+
+            remaining = int(response.headers['RateLimit-Remaining'])
             if remaining < 1:
-                print(f"approaching rate limit {remaining}/{response.headers['X-RateLimit-Limit-Second']}")
+                print(f"approaching rate limit {remaining}/{response.headers['RateLimit-Limit']}")
 
 
 
@@ -73,21 +77,26 @@ with open(f'account-events-{account}.csv', 'w', newline='') as csvfile:
             print(f"scanning block {block} ({progress*100}%) page : {page} - nr of events: {len(events)}, eta {t_togo/3600}h")
    
             for event in events:
-                if event['event_id'] == 'Transferred':
-                    print(event)
-                    params = json.loads(event['params'])
-                    cid = params[0]['value']
-                    account_from = ss58_encode(params[1]['value'], ss58_format=2)
-                    account_to = ss58_encode(params[2]['value'], ss58_format=2) 
-                    amount = params[3]['value']
-
-                    writer.writerow([cid,
-                                    account_from,
-                                    account_to, 
-                                    str(amount),
-                                    str(block),
-                                    ])
-                    csvfile.flush()
+                if event['module_id'] == 'encointerbalances':         
+                    if event['event_id'] == 'Transferred':
+                        #print(event)
+                        params = json.loads(event['params'])
+                        cid_raw = params[0]['value']
+                        cid = cid_raw['geohash'] + b58encode(bytearray.fromhex(cid_raw['digest'][2:])).decode("utf-8")
+                        account_from = ss58_encode(params[1]['value'], ss58_format=2)
+                        account_to = ss58_encode(params[2]['value'], ss58_format=2) 
+                        amount = params[3]['value']
+                        date = datetime.fromtimestamp(event['block_timestamp'])
+                        writer.writerow([
+                            date,
+                            cid,
+                            account_from,
+                            account_to, 
+                            str(amount),
+                            event['event_index'],
+                            event['extrinsic_hash']
+                        ])
+                        csvfile.flush()
 
             if len(events) < page_rows:
                 break
