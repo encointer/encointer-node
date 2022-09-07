778bca8b3317c6142f86ae3d2a36fc3cb80fa1f1
Alain Brenzikofer <news@brenzi.ch>
2022-09-07 09:18:11 +0200

    fetch helper logs 'Issued' events too

--- client/fetch-account-history.py
+++ client/fetch-account-history.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #
-# ./fetch-account-history.py <account> <subscan api key>
+# ./fetch-account-history.py <subscan api key>
 #
 import requests
 import csv
@@ -11,11 +11,6 @@ from datetime import datetime
 from substrateinterface.utils.ss58 import ss58_decode, ss58_encode
 from base58 import b58encode
 
-#if len(sys.argv) < 2:
-#    print("Usage: ./fetch-account-history.py <account> <subscan api key>")
-#    sys.exit()
-
-#account = sys.argv[1]
 account = "all"
 api_key = sys.argv[1]
 
@@ -23,8 +18,9 @@ page_rows = 100
 
 t_start = time.time()
 
-start_block = 535620
-end_block = 781918 #660815
+# TODO: edit these numbers to what you need. should be turned into cli arguments with defaults to 0..HEAD
+start_block = 1111286
+end_block = 1204517
 blocks_total = end_block - start_block
 
 with open(f'account-events-{account}-{start_block}_to_{end_block}.csv', 'w', newline='') as csvfile:
@@ -33,7 +29,7 @@ with open(f'account-events-{account}-{start_block}_to_{end_block}.csv', 'w', new
     for block in range(start_block, end_block):
         page = 0
         while True:
-            try: 
+            try:
                 response = requests.post('https://encointer.api.subscan.io/api/scan/events',
                                      headers={
                                          'Content-Type': 'application/json',
@@ -68,29 +64,41 @@ with open(f'account-events-{account}-{start_block}_to_{end_block}.csv', 'w', new
             progress = float(block - start_block + 1) / blocks_total
             t_elapsed = time.time() - t_start
             t_togo = t_elapsed / progress
-            print(f"scanning block {block}")
 
             if events is None:
-                break
+                events = []
 
-            print(f"scanning block {block} ({progress*100}%) page : {page} - nr of events: {len(events)}, eta {t_togo/3600}h")
-   
+            print(f"scanning block {block} ({round(progress*100)}%) page : {page} - nr of events: {len(events)}, eta {t_togo/3600}h")
+
             for event in events:
-                if event['module_id'] == 'encointerbalances':         
+                if event['module_id'] == 'encointerbalances':
+                    noteworthy=False
                     if event['event_id'] == 'Transferred':
-                        #print(event)
                         params = json.loads(event['params'])
                         cid_raw = params[0]['value']
                         cid = cid_raw['geohash'] + b58encode(bytearray.fromhex(cid_raw['digest'][2:])).decode("utf-8")
                         account_from = ss58_encode(params[1]['value'], ss58_format=2)
-                        account_to = ss58_encode(params[2]['value'], ss58_format=2) 
+                        account_to = ss58_encode(params[2]['value'], ss58_format=2)
                         amount = params[3]['value']
                         date = datetime.fromtimestamp(event['block_timestamp'])
+                        noteworthy = True
+
+                    if event['event_id'] == 'Issued':
+                        params = json.loads(event['params'])
+                        cid_raw = params[0]['value']
+                        cid = cid_raw['geohash'] + b58encode(bytearray.fromhex(cid_raw['digest'][2:])).decode("utf-8")
+                        account_from = 'community-issued-income'
+                        account_to = ss58_encode(params[1]['value'], ss58_format=2)
+                        amount = params[2]['value']
+                        date = datetime.fromtimestamp(event['block_timestamp'])
+                        noteworthy = True
+
+                    if noteworthy:
                         writer.writerow([
                             date,
                             cid,
                             account_from,
-                            account_to, 
+                            account_to,
                             str(amount),
                             event['event_index'],
                             event['extrinsic_hash']
