fbc1e6ea1dc7c5be6f89efaa2fdce6cecf13af4c
clangenb <37865735+clangenb@users.noreply.github.com>
2022-10-13 15:33:30 +0200

    [client] update dump_update_account_balances (#258)
    * client/dump_account_balances: update cid for leu
    
    * client/dump_account_balances: update block hash
    
    * [client/dump_account_balances] fix block_hash

--- client/dump_account_balances.sh
+++ client/dump_account_balances.sh
@@ -1,6 +1,6 @@
 cat accounts_all.txt | while read a
 do
-  BAL=$(../target/release/encointer-client-notee -u wss://kusama.api.encointer.org -p 443 balance $a --cid u0qj9QqA2Q --at 0xe3310c3d23bb95a618f72cca98a512c1e928923e80997bd29d121bc66bcb8a86)
+  BAL=$(../target/release/encointer-client-notee -u wss://kusama.api.encointer.org -p 443 balance $a --cid u0qj944rhWE --at 0x64e356817cefc0aa9353cac6d6e28169b9887b1e5df4535c218a7b553513e875)
   # translate Kusama prefix to substrate prefix for easy search
   ACC=$(subkey inspect $a --network substrate | grep -P '^[\s]+SS58' | sed 's/^ *SS58 Address: *//')
   echo $a,$ACC,$BAL
