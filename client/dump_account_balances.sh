b195c70c1a19e12431c703addebabaaaee152ad1
Christian Langenbacher <clangenb+gh@protonmail.ch>
2022-10-13 10:39:38 +0200

    client/dump_account_balances: update block hash

--- client/dump_account_balances.sh
+++ client/dump_account_balances.sh
@@ -1,6 +1,6 @@
 cat accounts_all.txt | while read a
 do
-  BAL=$(../target/release/encointer-client-notee -u wss://kusama.api.encointer.org -p 443 balance $a --cid u0qj944rhWE --at 0xe3310c3d23bb95a618f72cca98a512c1e928923e80997bd29d121bc66bcb8a86)
+  BAL=$(../target/release/encointer-client-notee -u wss://kusama.api.encointer.org -p 443 balance $a --cid u0qj944rhWE --at 0xa6f22967a9b642ce67bfb473889c899be918e94fee35794227617260a9eea811)
   # translate Kusama prefix to substrate prefix for easy search
   ACC=$(subkey inspect $a --network substrate | grep -P '^[\s]+SS58' | sed 's/^ *SS58 Address: *//')
   echo $a,$ACC,$BAL
