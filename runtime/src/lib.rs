26bd898e3cc9e6d07f3a66bc8f6046971e92b007
Christian Langenbacher <clangenb+gh@protonmail.ch>
2022-11-09 21:36:10 +0100

    Bump versions: * spec version because of new endorsement mechanism with reputables new `IssuedRewards` storage * bump minor version because of new endorsement mechanism

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -124,13 +124,8 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("encointer-node-notee"),
 	impl_name: create_runtime_str!("encointer-node-notee"),
 	authoring_version: 0,
-
-	// The version of the runtime specification. A full node will not attempt to use its native
-	//   runtime in substitute for the on-chain Wasm runtime unless all of `spec_name`,
-	//   `spec_version`, and `authoring_version` are the same between Wasm and native.
-	spec_version: 19,
+	spec_version: 20,
 	impl_version: 0,
-
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 2,
 	state_version: 0,
