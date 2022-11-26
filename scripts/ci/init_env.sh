3c1690ef685dae0bc736b128e7c6d9152e258232
Christian Langenbacher <clangenb+gh@protonmail.ch>
2022-11-26 11:49:22 +0100

    [scripts/ci] fix env vars

--- scripts/ci/init_env.sh
+++ scripts/ci/init_env.sh
@@ -3,7 +3,8 @@ set -euo pipefail
 
 # script that sets the correct environment variables to execute other scripts
 
-export SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+export CI_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+export SCRIPT_DIR="$(dirname "$CI_DIR")"
 export PROJ_ROOT="$(dirname "$SCRIPT_DIR")"
 export CLIENT_DIR="$PROJ_ROOT/client"
 export CLIENT_BIN="$PROJ_ROOT/target/release/encointer-client-notee"
@@ -11,7 +12,8 @@ export NODE_BIN="$PROJ_ROOT/target/release/encointer-node-notee"
 
 
 echo "Set environment variables:"
-echo "  BASH_SCRIPT_DIR: $SCRIPT_DIR"
+echo "  BASH_DIR: $CI_DIR"
+echo "  SCRIPT_DIR: $SCRIPT_DIR"
 echo "  PROJ_ROOT: $PROJ_ROOT"
 echo "  Client Directory: $CLIENT_DIR"
 echo "  Cleint Binary: $CLIENT_BIN"
