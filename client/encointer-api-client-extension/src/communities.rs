d790ac1019064661cf201365b93883229cedbdc6
haerdib <bhaerdi@devsgx02.scs-ad.scs.ch>
2023-06-13 19:03:59 +0200

    update to polkadot branch and remove unneeded option

--- client/encointer-api-client-extension/src/communities.rs
+++ client/encointer-api-client-extension/src/communities.rs
@@ -1,6 +1,6 @@
 use crate::{Api, Result};
 use encointer_primitives::communities::{CommunityIdentifier, Location};
-use substrate_api_client::{api::error::Error as ApiClientError, rpc::Request, rpc_params};
+use substrate_api_client::{rpc::Request, rpc_params};
 
 pub trait CommunitiesApi {
 	fn get_locations(&self, cid: CommunityIdentifier) -> Result<Vec<Location>>;
@@ -10,12 +10,7 @@ impl CommunitiesApi for Api {
 	fn get_locations(&self, cid: CommunityIdentifier) -> Result<Vec<Location>> {
 		let locations = self
 			.client()
-			.request::<Option<Vec<Location>>>("encointer_getLocations", rpc_params![cid])?
-			.ok_or_else(|| {
-				ApiClientError::Other(
-					format!("No locations founds. Does the cid {cid} exist").into(),
-				)
-			})?;
+			.request::<Vec<Location>>("encointer_getLocations", rpc_params![cid])?;
 		Ok(locations)
 	}
 }
