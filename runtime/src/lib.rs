a712e440fd18d23bea83e19fc9cb3a7b90480f99
Alain Brenzikofer <alain@integritee.network>
2023-06-16 18:14:18 +0200

    bump v1.4.0

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -130,7 +130,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("encointer-node-notee"),
 	impl_name: create_runtime_str!("encointer-node-notee"),
 	authoring_version: 0,
-	spec_version: 23,
+	spec_version: 24,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 3,
