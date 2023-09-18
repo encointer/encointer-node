4110d3c2799ba697bc3ea545cf57393dd3d803ba
Christian Langenbacher <clangenb+gh@protonmail.ch>
2023-09-18 14:03:35 +0200

    spec version v29, node/client version: 1.5.3

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -131,7 +131,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("encointer-node-notee"),
 	impl_name: create_runtime_str!("encointer-node-notee"),
 	authoring_version: 0,
-	spec_version: 28,
+	spec_version: 29,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 5,
