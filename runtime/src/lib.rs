a127ea7c363d711c346be73d54a8ff7b78e88564
Christian Langenbacher <clangenb+gh@protonmail.ch>
2023-09-22 10:36:53 +0200

    [runtime] fix type of existential deposit

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -419,7 +419,7 @@ parameter_types! {
 	pub const MomentsPerDay: Moment = 86_400_000; // [ms/d]
 	pub const DefaultDemurrage: Demurrage = Demurrage::from_bits(0x0000000000000000000001E3F0A8A973_i128);
 	/// 0.000005
-	pub const EncointerExistentialDeposit: BalanceType = BalanceType::from_bits(0x0000000000000000000053e2d6238da4_i128);
+	pub const EncointerExistentialDeposit: BalanceType = BalanceType::from_bits(0x0000000000000000000053e2d6238da4_u128);
 	pub const MeetupSizeTarget: u64 = 10;
 	pub const MeetupMinSize: u64 = 3;
 	pub const MeetupNewbieLimitDivider: u64 = 2;
