9a72eca7c59923f5cc53cd6d7d637d4e8b7094bd
Alain Brenzikofer <alain@integritee.network>
2024-02-21 13:35:22 +0100

    fixing transfer_keep_alive and origin for create community on parachain

--- scripts/install_python_deps.sh
+++ scripts/install_python_deps.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-python -m pip install --upgrade pip
+python3 -m pip install --upgrade pip
 pip install geojson pyproj RandomWords wonderwords requests flask substrate-interface click
