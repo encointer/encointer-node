f0d4eac3678e04bd47bdce1863cc2dd5c8b56096
Alain Brenzikofer <alain@integritee.network>
2024-09-10 09:03:33 +0200

    lift patches and bump crate versions

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -137,7 +137,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("encointer-node-notee"),
 	impl_name: create_runtime_str!("encointer-node-notee"),
 	authoring_version: 0,
-	spec_version: 350,
+	spec_version: 360,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 5,
