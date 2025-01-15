a84b16245955733edb4a70a4f2a2f8675b3386a7
Christian Langenbacher <clangenb+gh@protonmail.ch>
2025-01-15 13:44:20 +0100

    fix documentation and remove unnecessary port exposure

--- scripts/docker/entryscript.sh
+++ scripts/docker/entryscript.sh
@@ -14,8 +14,8 @@ case $1 in
     ;;
 
   bot-community-test)
-    # Example: note the port mapping, because we run the faucet at port 5000 in another container ...
-    # docker run -it -p 5005:5000 --add-host host.docker.internal:host-gateway test-client bot-community-test -r ws://host.docker.internal --port 9944 -f http://host.docker.internal:5000/api
+    # Example:
+    # docker run -it --add-host host.docker.internal:host-gateway test-client bot-community-test -u ws://host.docker.internal --port 9944 -f http://host.docker.internal:5000/api
 
     /bot-community.py --client /encointer-client-notee $PARAMS init
     /bot-community.py --client /encointer-client-notee $PARAMS test
@@ -23,26 +23,21 @@ case $1 in
     ;;
 
   phase.py)
-    # Example: note the port mapping, because we run another the faucet exposing 5000
-    # docker run -it -p 5001:5000 --add-host host.docker.internal:host-gateway test-client phase.py -r ws://host.docker.internal --port 9944 --idle-blocks 3
+    # Example:
+    # docker run -it --add-host host.docker.internal:host-gateway test-client phase.py -u ws://host.docker.internal --port 9944 --idle-blocks 3
     /phase.py --client /encointer-client-notee $PARAMS
     ;;
 
   faucet.py)
-    # Example:
+    # Example: Note: we have to expose the port
     # docker run -it -p 5000:5000 --add-host host.docker.internal:host-gateway test-client faucet.py -u ws://host.docker.internal --port 9944
     /faucet.py --client /encointer-client-notee $PARAMS
     ;;
 
-# not working yet, bot-commynity, and egister-random-businesses-and-offering have different interface. It is a pain.
+# Todo #386: Not working yet; bot-community, and register-random-businesses-and-offering have different interface.
 #  test-register-businesses)
 #    /bot-community.py --client /encointer-client-notee $PARAMS init
 #    /register-random-businesses-and-offerings.py --client /encointer-client-notee $PARAMS
-#    ;;
-
-# Does not work yet because the script wants the options like: cli.py --client <client> -u url -p port <cmd> <command params>
-#  cli.py)
-#    /cli.py $PARAMS
 #    ;;
 
   *)
