16c189fbbe515d8266467acd3f6736554e2c7f49
Christian Langenbacher <clangenb+gh@protonmail.ch>
2025-01-21 13:53:33 +0100

    [client] fix facuet typo

--- client/py_client/client.py
+++ client/py_client/client.py
@@ -272,22 +272,22 @@ class Client:
         ret = self.run_cli_command(["purge-community-ceremony", str(from_cindex), str(to_cindex)], cid, pay_fees_in_cc)
         return ret.stdout.decode("utf-8").strip()
 
-    def create_faucet(self, account, facuet_name, amount, drip_amount, whitelist, cid=None, pay_fees_in_cc=False):
-        ret = self.run_cli_command(["create-faucet", account, facuet_name, str(amount), str(drip_amount)] + whitelist,
+    def create_faucet(self, account, faucet_name, amount, drip_amount, whitelist, cid=None, pay_fees_in_cc=False):
+        ret = self.run_cli_command(["create-faucet", account, faucet_name, str(amount), str(drip_amount)] + whitelist,
                                    cid, pay_fees_in_cc)
         return ret.stdout.decode("utf-8").strip()
 
-    def drip_faucet(self, account, facuet_account, cindex, cid=None, pay_fees_in_cc=False):
-        ret = self.run_cli_command(["drip-faucet", account, facuet_account, str(cindex)], cid, pay_fees_in_cc)
+    def drip_faucet(self, account, faucet_account, cindex, cid=None, pay_fees_in_cc=False):
+        ret = self.run_cli_command(["drip-faucet", account, faucet_account, str(cindex)], cid, pay_fees_in_cc)
         return ret.stdout.decode("utf-8").strip()
 
-    def dissolve_faucet(self, account, facuet_account, beneficiary, cid=None, pay_fees_in_cc=False):
-        ret = self.run_cli_command(["dissolve-faucet", "--signer", account, facuet_account, beneficiary], cid,
+    def dissolve_faucet(self, account, faucet_account, beneficiary, cid=None, pay_fees_in_cc=False):
+        ret = self.run_cli_command(["dissolve-faucet", "--signer", account, faucet_account, beneficiary], cid,
                                    pay_fees_in_cc)
         return ret.stdout.decode("utf-8").strip()
 
-    def close_faucet(self, account, facuet_account, cid=None, pay_fees_in_cc=False):
-        ret = self.run_cli_command(["close-faucet", account, facuet_account], cid, pay_fees_in_cc)
+    def close_faucet(self, account, faucet_account, cid=None, pay_fees_in_cc=False):
+        ret = self.run_cli_command(["close-faucet", account, faucet_account], cid, pay_fees_in_cc)
         return ret.stdout.decode("utf-8").strip()
 
     def set_faucet_reserve_amount(self, account, amount, cid=None, pay_fees_in_cc=False):
